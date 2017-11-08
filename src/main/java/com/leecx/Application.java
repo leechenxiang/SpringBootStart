a40a84cfbdda2c2e35138732f722aff7929b9fc7
李晨翔 <leechenxiang@163.com>
2017-11-08 11:22:20 +0800

    springboot整合定时任务

--- src/main/java/com/leecx/Application.java
+++ src/main/java/com/leecx/Application.java
@@ -4,10 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+// 扫描 mybatis mapper 包路径
 @MapperScan(basePackages = "com.leecx.mapper")
+// 扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages= {"com.leecx", "org.n3r.idworker"})
+// 开启定时任务
+@EnableScheduling
 public class Application {
 
 	
---
+++ src/main/java/com/leecx/tasks/TestTask.java
@@ -0,0 +1,20 @@
+package com.leecx.tasks;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTask {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	
+	// 定义每过5秒执行任务
+//    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron="*/5 * * * * *")
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+}
